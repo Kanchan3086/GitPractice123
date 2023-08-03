2784cfc072faa29dff492d1f14817a3ac689e7b0
Kanchan <Kanchan@ANAIRA23PC>
2023-08-02 22:01:45 -0400

    first commit

---
+++ src/practiceJavaPackage1/Hellow_World.java
@@ -0,0 +1,11 @@
+package practiceJavaPackage1;
+
+public class Hellow_World {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is my first program Hellow World!!!");
+
+	}
+
+}
---
+++ src/practiceJavaPackage1/Pract_For_Loop.java
@@ -0,0 +1,19 @@
+package practiceJavaPackage1;
+
+public class Pract_For_Loop {
+
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "kanchan";
+		
+		for(int i=0;i<=10;i++)
+		{
+			System.out.println("My name is " + str);
+		}
+
+	}
+
+}
